db817907a9c063950087457e7462d258f3dd5d18
animalman33 <animalman3333@gmail.com>
2022-03-04 15:39:55 -0500

    added route for login info to be sent to #24

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 # route is used to server login pages
 @app.route("/")
-def root():
+def rootRoute():
     return render_template('login.html', title="Login")
 
 
 # route used to server user interface TODO add in check for if post or get and add in below
 @app.route("/user")
-def user():
+def userRoute():
     #TODO below for get request
     #TODO add checks for auth and redirect to root if not
     #TODO add checks for if admin
@@ -20,6 +20,12 @@ def user():
 
     return render_template("user.html", title="User")
 
+
+@app.route("/login", methods=["POST"])
+def loginRoute():
+    print(request.form["username"])
+    print(request.form["password"])
+    return request.form["username"]
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
