b511a3820fc544a4b975e98b144b0c06449ed342
animalman33 <animalman3333@gmail.com>
2022-03-04 15:00:39 -0500

    fixed issues with docker not running #10

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
 
 
-@app.route("/test")
-def test():
-    return render_template('test.html')
-
+# route is used to server login pages
 @app.route("/")
 def root():
-    return render_template('login.html', title = "Login")
+    return render_template('login.html', title="Login")
 
 
+# route used to server user interface TODO add in check for if post or get and add in below
 @app.route("/user")
 def user():
+    #TODO below for get request
     #TODO add checks for auth and redirect to root if not
     #TODO add checks for if admin
-    #TODO make page dynamic
     #TODO make title the username
-    return render_template("user.html", title = "User")
+    #TODO add redirect for invalid user
+
+    return render_template("user.html", title="User")
+
 
 if __name__ == "__main__":
-    app.run(port = 8080, debug = True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
