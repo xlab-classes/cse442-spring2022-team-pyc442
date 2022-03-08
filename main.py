ddf33c2b46b50791a049df0a9510b5b52574e3d0
animalman33 <animalman3333@gmail.com>
2022-03-08 12:30:08 -0500

    Added in flask login to keep track of users #24

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from flask import Flask, render_template, request, redirect
+from flask_login import LoginManager, login_required
+from src import User
+
 
 app = Flask(__name__)
 
+#flask login information
+loginManager = LoginManager()
+
+loginManager.init_app(app)
+loginManager.login_view = "/"
+
+@loginManager.user_loader
+def userLoader(userId):
+    #TODO add database information here
+    return NotImplementedError
 
 # route is used to server login pages
 @app.route("/")
@@ -11,8 +24,8 @@ def rootRoute():
 
 # route used to server user interface TODO add in check for if post or get and add in below
 @app.route("/user")
+@login_required
 def userRoute():
-    #TODO below for get request
     #TODO add checks for auth and redirect to root if not
     #TODO add checks for if admin
     #TODO make title the username
