e4904f33f0a5aa9d05da2529ea83efd93cf39a14
animalman33 <animalman3333@gmail.com>
2022-03-08 12:16:26 -0500

    Added test cases for authentication function #24

---
+++ tests/test_auth.py
@@ -0,0 +1,24 @@
+from src import auth
+from src.user import User
+from src.auth import authenticate
+
+def correctCreds():
+    retVal = authenticate("username", "password")
+    #Checks to make sure User object was returned
+    assert type(retVal) == User
+    #Checks to make sure username is correct within user object
+    assert retVal.get_username == "username"
+    #Checks to make sure the is_authenticated property works
+    assert retVal.is_authenticated == True
+    #Checks to make sure the is_active property is working
+    assert retVal.is_active == True
+
+def incorrectCreds():
+    retVal = authenticate("invalUser", "password")
+    assert retVal == None
+    retVal = authenticate("username", "")
+    assert retVal == None
+    retVal = authenticate("", "password")
+    assert retVal == None
+    retVal = authenticate("username", "invalidPass")
+    assert retVal == None
