050910774c063177a0f7d107444dcacd5ac008f8
animalman33 <animalman3333@gmail.com>
2022-03-08 14:08:39 -0500

    added login capabilities to login route #24

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request, redirect
-from flask_login import LoginManager, login_required
+from turtle import title
+from flask import Flask, render_template, request, redirect, flash
+from flask_login import LoginManager, login_required, login_user
 from src import User
+from src.auth import authenticate
 
 
 app = Flask(__name__)
@@ -28,19 +30,26 @@ def rootRoute():
 def userRoute():
     #TODO add checks for auth and redirect to root if not
     #TODO add checks for if admin
+    if User.is_admin:
+        return render_template("user.html", title="Admin")
     #TODO make title the username
     #TODO add redirect for invalid user
 
     return render_template("user.html", title="User")
 
 
+#Route to authenticate a user
 @app.route("/login", methods=["POST"])
 def loginRoute():
-    print(request.form["username"])
-    print(request.form["password"])
-    if(request.form["password"]=="wrong"):
-        return redirect("/login")
-    return redirect("/user")
+    # authenticates the user or returns none if invalid
+    user =  authenticate(request.form["username"], request.form["password"])
+    # Checks to make sure user was 
+    if(user != None):
+        login_user(user)
+        return redirect("/user")
+    else:
+        flash("Invalid password")
+        return
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
