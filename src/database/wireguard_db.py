f831570ac17b492672df3f6be955ea899c91f08a
Michael Lanurias <mlanuria@buffalo.com>
2022-03-15 17:18:30 -0400

    Updating create_database #26

--- src/database/wireguard_db.py
+++ src/database/wireguard_db.py
@@ -47,24 +47,33 @@ def create_database():
   except mysql.connector.Error as err:
       print("Failed creating database: {}".format(err))
 
-      cursor.execute("USE {}".format(DB_NAME))
+  try:
+    cursor.execute("USE {}".format(DB_NAME))
+  except mysql.connector.Error as err:
+    print("Database {} does not exists.".format(DB_NAME))
+    if err.errno == errorcode.ER_BAD_DB_ERROR:
+      try:
+        cursor.execute("CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(DB_NAME))
+      except mysql.connector.Error as err:
+        print("Failed creating database: {}".format(err))
+      print("Database {} created successfully.".format(DB_NAME))
 
       #checks if the table is existed
-      for table_name in TABLES:
-        table_description = TABLES[table_name]
-      try:
-          print("Creating table {}: ".format(table_name), end='')
-          cursor.execute(table_description)
-      except mysql.connector.Error as err:
+  for table_name in TABLES:
+    table_description = TABLES[table_name]
+    try:
+      print("Creating table {}: ".format(table_name), end='')
+      cursor.execute(table_description)
+    except mysql.connector.Error as err:
           if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
               print("already exists.")
           else:
               print(err.msg)
-      else:
-          print("OK")
+    else:
+      print("OK")
       
-      cursor.close()
-      cnx.close()
+    cursor.close()
+    cnx.close()
     
 #Adds users to the server
 def add_users(id, email, username, password, is_admin, is_banned):
@@ -319,7 +328,7 @@ if __name__ == "__main__":
     print(getUserById(id))
 
     print("Printing the user by the name...")
-    info1 = getUserByName(name)
+    print(getUserByName(name))
 
     print("Printing the new username...")
     print(modifyUsername(id, "new_username_442"))
