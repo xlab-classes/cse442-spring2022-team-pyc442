da25ec0fca339855c69cbd9b1d0325937729120f
Michael Lanurias <mlanuria@buffalo.edu>
2022-03-19 20:05:20 -0400

    Cleaned up the codes #44

--- src/database/wireguard_db.py
+++ src/database/wireguard_db.py
@@ -1,4 +1,3 @@
-from json.tool import main
 import pylint
 import mysql.connector
 from mysql.connector import errorcode
@@ -32,8 +31,7 @@ def create_database():
       "   `admin`      tinyint(1),"
       "   `banned`   tinyint(1),"
       "   PRIMARY KEY (`user_id`)"
-      ") ENGINE=InnoDB")
-      
+      ") ENGINE=InnoDB")      
     cnx = mysql.connector.connect(
         host="localhost",
         user="root",
@@ -52,165 +50,163 @@ def create_database():
     except mysql.connector.Error as err:
         print("Database {} does not exists.".format(DB_NAME))
         if err.errno == errorcode.ER_BAD_DB_ERROR:
-          try:
-            cursor.execute("CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(DB_NAME))
-          except mysql.connector.Error as err:
-            print("Failed creating database: {}".format(err))
-          print("Database {} created successfully.".format(DB_NAME))
+            try:
+                cursor.execute("CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(DB_NAME))
+            except mysql.connector.Error as err:
+                print("Failed creating database: {}".format(err))
+            print("Database {} created successfully.".format(DB_NAME))
 
       #checks if the table is existed
     for table_name in TABLES:
-      table_description = TABLES[table_name]
-      try:
-        print("Creating table {}: ".format(table_name), end='')
-        cursor.execute(table_description)
-      except mysql.connector.Error as err:
+        table_description = TABLES[table_name]
+        try:
+            print("Creating table {}: ".format(table_name), end='')
+            cursor.execute(table_description)
+        except mysql.connector.Error as err:
             if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
                 print("already exists.")
             else:
                 print(err.msg)
-      else:
-        print("OK")
-      
-      cursor.close()
-      cnx.close()
-    
+        else:
+            print("OK")
+    cursor.close()
+    cnx.close()
 #Adds users to the server
-def add_users(id, email, username, password, is_admin, is_banned):
-      cnx = mysql.connector.connect( # connceting to database
+def add_users(uid, email, username, password, is_admin, is_banned):
+    cnx = mysql.connector.connect( # connceting to database
       host="localhost",
       user="root",
       password="FalaWB@321",
       database="wireguard"
       )
 
-      cursor = cnx.cursor()
+    cursor = cnx.cursor()
 
-      add_user = ("INSERT INTO wireguard "
+    add_user = ("INSERT INTO wireguard "
                 "(user_id, email, username, password, admin, banned) "
                 "VALUES (%s, %s, %s, %s, %s, %s)") # query to add user
 
-      data_user = (id, email, username, password, is_admin, is_banned)
+    data_user = (uid, email, username, password, is_admin, is_banned)
 
-      cursor.execute(add_user, data_user)
+    cursor.execute(add_user, data_user)
 
-      cnx.commit()
+    cnx.commit()
 
-      cursor.close()
-      cnx.close()
+    cursor.close()
+    cnx.close()
 
     #Execute the general query
 def general_query():
-      cnx = mysql.connector.connect( 
+    cnx = mysql.connector.connect(
       host="localhost",
       user="root",
       password="FalaWB@321",
       database="wireguard"
       )
 
-      user_data = []
+    user_data = []
 
-      cursor = cnx.cursor()
+    cursor = cnx.cursor()
 
-      query = ("SELECT * FROM wireguard") 
+    query = ("SELECT * FROM wireguard") 
 
-      cursor.execute(query)
+    cursor.execute(query)
 
-      for (user_id, email, username, password, admin, banned) in cursor:
+    for (user_id, email, username, password, admin, banned) in cursor:
         user_data.append([user_id, email, username, password, admin, banned])
 
-      cursor.close()
-      cnx.close()
-      if user_data == []:
+    cursor.close()
+    cnx.close()
+    if user_data == []:
         return None
-      return user_data[0] #returning first element of user_data
+    return user_data[0] #returning first element of user_data
 
 #Gets a user entry by user id, returns user data as first element of list user_data
 def getUserById(uid):
-      cnx = mysql.connector.connect( #connecting to database
+    cnx = mysql.connector.connect( #connecting to database
         host="localhost",
         user="root",
         password="FalaWB@321",
         database="wireguard"
       )
 
-      user_data = [] #initializing list, first element will be returned
+    user_data = [] #initializing list, first element will be returned
 
-      cursor = cnx.cursor()
+    cursor = cnx.cursor()
 
-      query = ("SELECT * FROM wireguard WHERE user_id = %s") #writing query to find user by uid
+    query = ("SELECT * FROM wireguard WHERE user_id = %s") #writing query to find user by uid
 
-      user_id = uid
+    user_id = uid
 
-      cursor.execute(query, (user_id,))
+    cursor.execute(query, (user_id,))
 
-      for (user_id, email, username, password, admin, banned) in cursor: # for loop to poulate user_data
+    for (user_id, email, username, password, admin, banned) in cursor: # to populate user_data
         user_data.append([user_id, email, username, password, admin, banned])
 
-      cursor.close()
-      cnx.close()
+    cursor.close()
+    cnx.close()
 
-      if user_data == []:
+    if user_data == []:
         return None
-      return user_data[0] #returning first element of user_data
+    return user_data[0] #returning first element of user_data
 
 #Gets a user entry by username, returns user data as first element of list user_data
 def getUserByName(uname):
-      cnx = mysql.connector.connect( #connecting to database
+    cnx = mysql.connector.connect( #connecting to database
         host="localhost",
         user="root",
         password="FalaWB@321",
         database="wireguard"
       )
 
-      user_data = [] #initializing list, first element will be returned
+    user_data = [] #initializing list, first element will be returned
 
-      cursor = cnx.cursor()
+    cursor = cnx.cursor()
 
-      query = ("SELECT * FROM wireguard WHERE username = %s") #writing query to find a user by username
+    query = ("SELECT * FROM wireguard WHERE username = %s") #find a user by username
 
-      cursor.execute(query, (uname,))
+    cursor.execute(query, (uname,))
 
-      for (user_id, email, username, password, admin, banned) in cursor: # for loop to poulate user_data
+    for (user_id, email, username, password, admin, banned) in cursor: #populate user_data
         user_data.append([user_id, email, username, password, admin, banned])
 
-      cursor.close()
-      cnx.close()
-      if user_data == []:
+    cursor.close()
+    cnx.close()
+    if user_data == []:
         return None
-      return user_data[0] #returning first element of user_data
+    return user_data[0] #returning first element of user_data
 
 #change a user's username to newUname, returns user data as first element of list user_data
 def modifyUsername(uid, newUname):
-      cnx = mysql.connector.connect( # connecting to database
+    cnx = mysql.connector.connect( # connecting to database
         host="localhost",
         user="root",
         password="FalaWB@321",
         database="wireguard"
       )
 
-      user_data = [] # initializing list, first element will be returned
+    user_data = [] # initializing list, first element will be returned
 
-      cursor = cnx.cursor()
+    cursor = cnx.cursor()
 
-      query = ("UPDATE wireguard SET username = %s WHERE user_id = %s") #writing query to update username
+    query = ("UPDATE wireguard SET username = %s WHERE user_id = %s") #to update username
 
-      cursor.execute(query, (newUname, uid))
+    cursor.execute(query, (newUname, uid))
 
-      cnx.commit()
+    cnx.commit()
 
-      query = ("SELECT * FROM wireguard WHERE user_id = %s") #write query to get user data
+    query = ("SELECT * FROM wireguard WHERE user_id = %s") #write query to get user data
 
-      cursor.execute(query, (uid,))
+    cursor.execute(query, (uid,))
 
-      for user_id, email, username, password, admin, banned in cursor: #for loop to populate user_data
+    for user_id, email, username, password, admin, banned in cursor: #for loop to populate user_data
         user_data.append([user_id, email, username, password, admin, banned])
 
-      cursor.close()
-      cnx.close()
-      if user_data == []:
+    cursor.close()
+    cnx.close()
+    if user_data == []:
         return None
-      return user_data[0] #returning first element of user_data # returning first element of user_data 
+    return user_data[0] #returning first element of user_data # returning first element of user_data
 
 #change a user's ban status, returns user data as first element of list user_data
 def changeBannedStatus(uid, newBanStatus):
@@ -224,7 +220,7 @@ def changeBannedStatus(uid, newBanStatus):
 
     cursor = cnx.cursor()
 
-    query = ("UPDATE wireguard SET banned = %s WHERE user_id = %s") #writing query to update ban status
+    query = ("UPDATE wireguard SET banned = %s WHERE user_id = %s") #to update ban status
 
     cursor.execute(query, (newBanStatus, uid))
 
@@ -234,14 +230,14 @@ def changeBannedStatus(uid, newBanStatus):
 
     cursor.execute(query, (uid,))
 
-    for (user_id, email, username, password, admin, banned) in cursor: #for loop to populate user_data
-      user_data.append([user_id, email, username, password, admin, banned])
+    for (user_id, email, username, password, admin, banned) in cursor: #populate user_data
+        user_data.append([user_id, email, username, password, admin, banned])
 
     cursor.close()
     cnx.close()
 
     if user_data == []:
-      return None
+        return None
     return user_data[0] #returning first element of user_data
 
 def deleteUserByName(name):
@@ -260,14 +256,14 @@ def deleteUserByName(name):
 
     cursor.execute(query2, (name,))
 
-    for (user_id, email, username, password, admin, banned) in cursor: #for loop to populate user_data
-      user_data.append((user_id, email, username, password, admin, banned))
+    for (user_id, email, username, password, admin, banned) in cursor: #populate user_data
+        user_data.append((user_id, email, username, password, admin, banned))
 
     if user_data == []:
-      cursor.close()
-      cnx.close()
-      return False
-    
+        cursor.close()
+        cnx.close()
+        return False
+
     query1 = ("DELETE FROM wireguard WHERE username = %s")
 
     cursor.execute(query1, (name,))
@@ -279,38 +275,23 @@ def deleteUserByName(name):
     cnx.close()
 
     return True
-    
 
-
-
-
-
-#currently deletes all entries from the database, change to only delete a specified tuple from the database
+#deletes all entries from the database
 def deleteAllTuples():
-      cnx = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="FalaWB@321",
-        database="wireguard"
-      )
+    cnx = mysql.connector.connect(
+      host="localhost",
+      user="root",
+      password="FalaWB@321",
+      database="wireguard"
+    )
 
-      cursor = cnx.cursor()
+    cursor = cnx.cursor()
 
-      query = ("DELETE FROM wireguard")
+    query = ("DELETE FROM wireguard")
 
-      cursor.execute(query)
+    cursor.execute(query)
 
-      cnx.commit()
+    cnx.commit()
 
-      cursor.close()
-      cnx.close()
-
-
-#testing
-if __name__ == "__main__":
-      add_users("1", "any@any.com", "username", bcrypt.hashpw(b"password", bcrypt.gensalt()), 1, 0)
-      add_users("2", "any2@any2.com", "changed_20", bcrypt.hashpw(b"password2", bcrypt.gensalt()), 0, 1)
-      add_users("3", "any3@any3.com", "username3", bcrypt.hashpw(b"password3", bcrypt.gensalt()), 0, 0)
-      print(deleteUserByName("username"))
-      print(deleteUserByName("changed_20"))
-      print(deleteUserByName("username3"))
+    cursor.close()
+    cnx.close()
--- tests/test_db.py
+++ tests/test_db.py
@@ -1,58 +1,58 @@
 import pytest
 import bcrypt
-from src.database.wireguard_db import add_users, changeBannedStatus, deleteUserByName, general_query, getUserById, modifyUsername, setup_db, create_database, deleteAllTuples, getUserByName
 import pylint
+import src.database.wireguard_db as db
 
 def test_adding_more_users():
-    deleteAllTuples()
-    setup_db()
-    create_database()
+    db.deleteAllTuples()
+    db.setup_db()
+    db.create_database()
 
-    add_users("1", "any@any.com", "username", bcrypt.hashpw(b"password", bcrypt.gensalt()), 1, 0)
-    add_users("2", "any2@any.com", "username2", bcrypt.hashpw(b"password2", bcrypt.gensalt()), 0, 0)
-    add_users("3", "any3@any.com", "username3", bcrypt.hashpw(b"password3", bcrypt.gensalt()), 0, 1)
+    db.add_users("1", "any@any.com", "username", bcrypt.hashpw(b"pwd", bcrypt.gensalt()), 1, 0)
+    db.add_users("2", "any2@any.com", "username2", bcrypt.hashpw(b"pwd2", bcrypt.gensalt()), 0, 0)
+    db.add_users("3", "any3@any.com", "username3", bcrypt.hashpw(b"pwd3", bcrypt.gensalt()), 0, 1)
 
     #Checks to make sure the info is returned
-    test1 = general_query()
+    test1 = db.general_query()
     assert test1 is not None
 
 #tests for incorrect username on authenticate function
 def test_for_user_id():
     #Checks if the user by their id is there
-    test2 = getUserById("2")
+    test2 = db.getUserById("2")
     assert test2 is not None
     #Checks if it's not there
-    test3 = getUserById("4")
+    test3 = db.getUserById("4")
     assert test3 is None
 
 #tests for empty password on authenticate function
 def test_for_user_name():
-    test4 = getUserByName("username3")
+    test4 = db.getUserByName("username3")
     assert test4 is not None
-    test5 = getUserByName("username10")
+    test5 = db.getUserByName("username10")
     assert test5 is None
 
 #tests for empty username on authenticate function
 def test_for_modification():
-    test6 = modifyUsername("2", "changed_20")
+    test6 = db.modifyUsername("2", "changed_20")
     assert test6 is not None
-    test7 = modifyUsername("5", "changed_25")
+    test7 = db.modifyUsername("5", "changed_25")
     assert test7 is None
 
 #tests for invalid password on authenticate function
 def test_banned_status():
-    test8 = changeBannedStatus("2", 1)
+    test8 = db.changeBannedStatus("2", 1)
     assert test8 is not None
-    test9 = changeBannedStatus("3", 0)
+    test9 = db.changeBannedStatus("3", 0)
     assert test9 is not None
 
 #tests for empty password and username on authenticate function
 def test_delete_user():
-    test10 = deleteUserByName("username")
+    test10 = db.deleteUserByName("username")
     assert test10 is True
-    test11 = deleteUserByName("changed_20")
+    test11 = db.deleteUserByName("changed_20")
     assert test11 is True
-    test12 = deleteUserByName("username3")
+    test12 = db.deleteUserByName("username3")
     assert test12 is True
-    test13 = deleteUserByName("username3")
+    test13 = db.deleteUserByName("username3")
     assert test13 is False
