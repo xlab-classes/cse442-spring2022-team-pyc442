cff7c6772b29906ddb1371e0dac4a6aac8178cb9
Michael Lanurias <mlanuria@buffalo.com>
2022-03-29 13:48:40 -0400

    Auth is fixing #34 & #44

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,7 +1,7 @@
+import bcrypt
 from src.authentication.user import User
 from src.authentication.auth import authenticate
 import src.database.wireguard_db as db
-import bcrypt
 
 #setup database and resets database for tests
 #@pytest.fixture()
@@ -21,53 +21,53 @@ def test_correct_creds():
     db.create_database()
 
     db.add_users("1", "any@any.com", "username", bcrypt.hashpw(b"password", bcrypt.gensalt()), 1, 0)
-    retVal = authenticate("username", "password")
+    rv1 = authenticate("username", "password")
     db.deleteAllTuples()
     #Checks to make sure User object was returned
-    assert retVal != None
+    assert rv1 is not None
     #Checks to make sure username is correct within user object
-    assert retVal.get_username() == "username"
+    assert rv1.get_username() == "username"
     #Checks to make sure the is_authenticated property works
-    assert retVal.is_authenticated() == True
+    assert rv1.is_authenticated() is True
     #Checks to make sure the is_active property is working
-    assert retVal.is_active() == False
+    assert rv1.is_active() == 0
 
 #tests for incorrect username on authenticate function
 def test_incorrect_username():
     db.deleteAllTuples()
     db.add_users("1", "any@any.com", "username", bcrypt.hashpw(b"password", bcrypt.gensalt()), 1, 0)
-    retVal = authenticate("invalUser", "password")
+    rv2 = authenticate("invalUser", "password")
     db.deleteAllTuples()
-    assert retVal == None
+    assert rv2 is None
 
 #tests for empty password on authenticate function
 def test_empty_pass():
     db.deleteAllTuples()
     db.add_users("1", "any@any.com", "username", bcrypt.hashpw(b"password", bcrypt.gensalt()), 1, 0)
-    retVal = authenticate("username", "")
+    rv3 = authenticate("username", "")
     db.deleteAllTuples()
-    assert retVal == None
+    assert rv3 is None
 
 #tests for empty username on authenticate function
 def test_empty_username():
     db.deleteAllTuples()
     db.add_users("1", "any@any.com", "username", bcrypt.hashpw(b"password", bcrypt.gensalt()), 1, 0)
-    retVal = authenticate("", "password")
+    rv4 = authenticate("", "password")
     db.deleteAllTuples()
-    assert retVal == None
+    assert rv4 is None
 
 #tests for invalid password on authenticate function
 def test_invalid_password():
     db.deleteAllTuples()
     db.add_users("1", "any@any.com", "username", bcrypt.hashpw(b"password", bcrypt.gensalt()), 1, 0)
-    retVal = authenticate("username", "invalidPass")
+    rv5 = authenticate("username", "invalidPass")
     db.deleteAllTuples()
-    assert retVal == None
+    assert rv5 is None
 
 #tests for empty password and username on authenticate function
 def test_empty_creds():
     db.deleteAllTuples()
     db.add_users("1", "any@any.com", "username", bcrypt.hashpw(b"password", bcrypt.gensalt()), 1, 0)
-    retVal = authenticate("","")
+    rv6 = authenticate("","")
     db.deleteAllTuples()
-    assert retVal == None
+    assert rv6 is None
