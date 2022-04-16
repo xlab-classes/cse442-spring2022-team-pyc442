8b0c3c3a1b4ded054cc2f71aac443b1890ac5072
animalman33 <animalman3333@gmail.com>
2022-04-16 16:10:32 -0400

    fixed setup of wireguard server #77

--- src/wireguard/first_start.sh
+++ src/wireguard/first_start.sh
@@ -20,11 +20,7 @@ DEV=$(ip route list default | awk '{print $5}')
 
 # the base configuration for the server
 # sorry for how hard this is to read but bash wasnt letting me do muliline strings
-CONFIG_FILE="[Interface]\nAddress = 10.8.0.1/24\nSaveConfig = true\nPostUp = iptables -t nat -I POSTROUTING -o $DEV -j MASQUERADE\nPostUp = iptables -I INPUT 1 -i wg0 -j ACCEPT\nPostUp = iptables -I FORWARD 1 -i $DEV -o wg0 -j ACCEPT\nPostUp = iptables -I FORWARD 1 -i wg0 -o $DEV -j ACCEPT\nPreDown = iptables -t nat -D POSTROUTING -o $DEV -j MASQUERADE\nPreDown = iptables -D INPUT -i wg0 -j ACCEPT\nPreDown = iptables -D FORWARD -i $DEV -o wg0 -j ACCEPT\nPreDown = iptables -D FORWARD -i wg0 -o $DEV -j ACCEPT\nListenPort = 51820\nPrivateKey = $PRIVKEY\n
-"
-
+CONFIG_FILE="[Interface]\nAddress = 10.8.0.1/32\nSaveConfig = true\nPostUp = ufw enable\nPostUp = sysctl -w net.ipv4.ip_forward=1\nPostUp = iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE\nPostUp = ufw route allow in on wg0 out on $DEV\nPreDown = ufw route delete allow in on wg0 out on $DEV\nPreDown = iptables -t nat -D POSTROUTING -o $DEV -j MASQUERADE\nPreDown = sysctl -w net.ipv4.ip_forward=0\nListenPort = 51820\nPrivateKey = $PRIVKEY\n"
 # write the config out to file
 echo -e $CONFIG_FILE > /etc/wireguard/wg0.conf
 
-#allow IPv4 forwarding
-sysctl -w net.ipv4.ip_forward=1
--- src/wireguard/wireguard_server.py
+++ src/wireguard/wireguard_server.py
@@ -19,14 +19,12 @@ class Wireguard_Server():
 
     def start(self) -> None:
         if not self._running:
-            subprocess.run(['sudo', 'sysctl', '-w', 'net.ipv4.ip_forward=1'])
             subprocess.run(['sudo', 'wg-quick', 'up', 'wg0'])
             self._running = True
 
 
     def stop(self) -> None:
         if self._running:
-            subprocess.run(['sudo', 'sysctl', '-w', 'net.ipv4.ip_forward=0'])
             subprocess.run(['sudo', 'wg-quick', 'down', 'wg0'])
             self._running = False
 
