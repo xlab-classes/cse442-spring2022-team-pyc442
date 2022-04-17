3870d89f621571e1264cf0ecea4588a25f1707a8
animalman33 <animalman3333@gmail.com>
2022-04-17 11:48:42 -0400

    removed newline character at in wireguard conf #77

--- src/wireguard/first_start.sh
+++ src/wireguard/first_start.sh
@@ -21,20 +21,20 @@ DEV=$(ip route list default | awk '{print $5}')
 # the base configuration for the server
 # write the config out to file
 cat > /etc/wireguard/wg0.conf << EOL
-[Interface]\n
-Address = 10.8.0.1/24\n
-SaveConfig = true\n
-PostUp = ufw route allow in on wg0 out on $DEV\n
-PostUp = ufw route allow in on $DEV out on wg0\n
-PostUp =  ufw allow proto udp from any to any port 51820\n
-PostUp = sysctl -w net.ipv4.ip_forward=1\n
-PostUp = iptables -t nat -I POSTROUTING -o $DEV -j MASQUERADE\n
-PreDown = iptables -t nat -D POSTROUTING -o $DEV -j MASQUERADE\n
-PostDown = ufw route delete allow in on wg0 out on eth0\n
-PostDown = ufw route delete allow in on eth0 out on wg0\n
-PostDown = ufw delete allow proto udp from any to any port 51820\n
-PreDown = sysctl -w net.ipv4.ip_forward=0\n
-ListenPort = 51820\n
-PrivateKey = $PRIVKEY\n
+[Interface]
+Address = 10.8.0.1/24
+SaveConfig = true
+PostUp = ufw route allow in on wg0 out on $DEV
+PostUp = ufw route allow in on $DEV out on wg0
+PostUp =  ufw allow proto udp from any to any port 51820
+PostUp = sysctl -w net.ipv4.ip_forward=1
+PostUp = iptables -t nat -I POSTROUTING -o $DEV -j MASQUERADE
+PreDown = iptables -t nat -D POSTROUTING -o $DEV -j MASQUERADE
+PostDown = ufw route delete allow in on wg0 out on eth0
+PostDown = ufw route delete allow in on eth0 out on wg0
+PostDown = ufw delete allow proto udp from any to any port 51820
+PreDown = sysctl -w net.ipv4.ip_forward=0
+ListenPort = 51820
+PrivateKey = $PRIVKEY
 EOL
 
