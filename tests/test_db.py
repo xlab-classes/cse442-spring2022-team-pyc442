4d8b734b442b31848af313ba8baf8358bb93a4aa
mrtrayno <mrtrayno@buffalo.edu>
2022-04-27 15:19:07 -0400

    #101 added function to get list of users and added tests for it

--- tests/test_db.py
+++ tests/test_db.py
@@ -106,3 +106,17 @@ def test_banned_users():
     assert test18[0] == "username"
     assert test18[1] == "username3"
     assert test18[2] == "username2"
+
+def test_user_list():
+    db.add_users("1", "any@any.com", "username", bcrypt.hashpw(b"pwd", bcrypt.gensalt()), 1, 0)
+    db.add_users("2", "any2@any.com", "username3", bcrypt.hashpw(b"pwd", bcrypt.gensalt()), 1, 0)
+    db.add_users("3", "any3@any.com", "username2", bcrypt.hashpw(b"pwd", bcrypt.gensalt()), 1, 0)
+
+    test18 = []
+    test18 = db.listUsers()
+
+    db.deleteAllTuples()
+
+    assert test18[0] == "username"
+    assert test18[1] == "username3"
+    assert test18[2] == "username2"
