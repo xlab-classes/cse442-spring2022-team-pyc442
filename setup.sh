9e856429677111da8fb6389ee4461ac4271c9759
animalman33 <animalman3333@gmail.com>
2022-05-01 22:18:48 -0400

    added the installation of packages #102

--- setup.sh
+++ setup.sh
@@ -4,8 +4,14 @@
 
 if [ $EUID -ne 0 ]
 then
-   echo "this script needs to be run as root"
+   echo "this script needs to be run as root or using sudo"
    exit
 fi
 
+# download and install all the packages necessary
+apt update
+apt install -y mysql-server virtualenv python3 python-is-python3 apache2 openssl wireguard
+
 sh setup/setup_database.sh
+sh setup/setup_user.sh
+python setup_admin_user.py
