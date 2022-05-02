80f815ec7b8cb230860d1498d0dd226eddb3faaa
animalman33 <animalman3333@gmail.com>
2022-05-01 23:17:54 -0400

    moved wireguard user password generation to setup.sh #102

--- setup/setup_database.sh
+++ setup/setup_database.sh
@@ -7,12 +7,10 @@ systemctl start mysql
 
 mysql_secure_installation -u root --use-default
 
-USER_PASSWD=$(gpg --gen-random --armor 1 14)
-
-echo "{\"username\": \"wireguard\", \"password\": \"$USER_PASSWD\"}" > database.cfg
+echo "{\"username\": \"wireguard\", \"password\": \"$1\"}" > database.cfg
 
 
-mysql -u root -e "CREATE USER IF NOT EXISTS 'wireguard'@'localhost' IDENTIFIED BY '$USER_PASSWD';"
+mysql -u root -e "CREATE USER IF NOT EXISTS 'wireguard'@'localhost' IDENTIFIED BY '$1';"
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS wireguard DEFAULT CHARACTER SET 'utf8';"
 mysql -u root -e "GRANT SELECT, INSERT, UPDATE, DELETE ON wireguard.* TO 'wireguard'@'localhost';"
 mysql -u root -e "FLUSH PRIVILEGES;"
